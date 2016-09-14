65fc24331196082fe8b23ad702a6583d024ee968
Ryan Hanson <hansonry@gmail.com>
2016-09-14 15:18:37 -0500

    Updated SDL version

--- bam.lua
+++ bam.lua
@@ -1,12 +1,26 @@
 settings = NewSettings()
 
+
 if family == "windows" then
-   settings.cc.includes:Add("SDL2-2.0.1/include");
+   if arch == "amd64" or arch == "ia64" then
+      lib_sdl_path = "SDL2-2.0.4/lib/x64"
+      lib_sdl_image_path = "SDL2_image-2.0.1/lib/x64"
+   else
+      lib_sdl_path = "SDL2-2.0.4/lib/x86"
+      lib_sdl_image_path = "SDL2_image-2.0.1/lib/x86"
+   end   
+   settings.cc.includes:Add("SDL2-2.0.4/include")
+   settings.cc.includes:Add("SDL2-2.0.4/include/SDL2")
+   settings.cc.includes:Add("SDL2_image-2.0.1/include")
+   settings.cc.includes:Add("SDL2_image-2.0.1/include/SDL2")
    settings.debug = 0
-   settings.cc.flags:Add("/MD");
-   settings.link.flags:Add("/SUBSYSTEM:CONSOLE");   
-   settings.link.libs:Add("SDL2main");
-   settings.link.libpath:Add("SDL2-2.0.1/lib/x86");
+   settings.cc.flags:Add("/MD")
+   settings.link.flags:Add("/SUBSYSTEM:CONSOLE")
+   settings.link.libs:Add("SDL2main")
+   settings.link.libpath:Add(lib_sdl_path)
+   settings.link.libpath:Add(lib_sdl_image_path)
+
+
 else
    settings.cc.flags:Add("-Wall");
 end
@@ -21,4 +35,14 @@ source = Collect("*.c");
 
 objects = Compile(settings, source)
 exe = Link(settings, "sdl2d", objects)
+
+if family == "windows" then
+   dll_copy = CopyToDirectory(".", Collect(lib_sdl_path .. "/*.dll", 
+                                           lib_sdl_image_path .. "/*.dll"))
+   PseudoTarget("all", exe, dll_copy)
+   DefaultTarget("all")
+else
+   DefaultTarget(exe)
+end
+
 
